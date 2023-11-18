27d3f7ba36ce602494f592b88be99985c1c978d9
João Alves <joaoalvescordeiro2005@gmail.com>
2023-11-18 15:52:28 -0400

    organização

--- Aulas/2-semestre/projeto-1/codigo/js/mobile-navbar.js
+++ Aulas/2-semestre/projeto-1/codigo/js/mobile-navbar.js
@@ -8,25 +8,29 @@ class MobileNavbar {
         this.handleClick = this.handleClick.bind(this);
     }
 
+    //Control Animate
     animateLinks() {
         this.navLinks.forEach((link, index) => {
             link.style.animation
                 ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.7s ease forwards ${index / 7 + 0.3}s`);
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`);
 
         });
     }
 
+    //Control Click in Menu
     handleClick() {
         this.navList.classList.toggle(this.activeClass);
         this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
     }
 
+    //Add Listener in Menu
     addClickEvent() {
         this.mobileMenu.addEventListener("click", this.handleClick);
     }
 
+    //Start
     init() {
         if (this.mobileMenu) {
             this.addClickEvent(); 
