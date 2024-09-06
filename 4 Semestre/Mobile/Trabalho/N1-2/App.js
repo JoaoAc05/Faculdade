991435c2e802a67b9bd7d17e19508463a1388a8b
João Alves <joaoalvescordeiro2005@gmail.com>
2024-09-06 15:06:07 -0400

    Trabalho mobile

--- 4 Semestre/Mobile/Trabalho/N1-2/App.js
+++ 4 Semestre/Mobile/Trabalho/N1-2/App.js
@@ -6,14 +6,40 @@ import { Button, View } from 'react-native';
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notificacoes')}
-        title="Ir para notificações"
-      />
+      <Button onPress={() => navigation.navigate('Shopping')} title="Fazer Compras" />  
+        <br/>     
+       <Button onPress={() => navigation.navigate('Carrinho')} title="Ver Carrinho" />
+        <br/>
+      <Button onPress={() => navigation.navigate('Favoritos')} title="Ver Favoritos" /> 
+        <br/>
+      <Button onPress={() => navigation.navigate('Notificacoes')} title="Ir para notificações" />
     </View>
   );
 }
 
+function ShoppingScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Ir para home" />
+    </View>
+  );
+}
+
+function CarrinhoScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Ir para home" />
+    </View>
+  );
+}
+
+function FavoritosScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Ir para home" />
+    </View>
+  );
+}
 function NotificacoesScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -29,6 +55,9 @@ export default function App() {
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Shopping" component={ShoppingScreen} />
+        <Drawer.Screen name="Carrinho" component={CarrinhoScreen} />
+        <Drawer.Screen name="Favoritos" component={FavoritosScreen} />
         <Drawer.Screen name="Notificacoes" component={NotificacoesScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
